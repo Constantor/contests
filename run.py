ddc440ad7882c2a02dbad8149cdc269e2b7705d3
Stepan Konstantinov <stepan@konstantinov.pl>
2023-06-09 23:06:54 +0200

    Minor fix

--- run.py
+++ run.py
@@ -11,28 +11,28 @@ scriptDir = os.path.realpath(os.path.dirname(__file__))
 id = ""
 target = ""
 if sys.argv[1] == "c":
-    target = "c_" + sys.argv[2] + "_"
-    id = sys.argv[3]
+	target = "c_" + sys.argv[2] + "_"
+	id = sys.argv[3]
 elif sys.argv[1] in ("raw", "temp", "kop"):
-    target = sys.argv[1]
-    id = sys.argv[2]
+	target = sys.argv[1]
+	id = sys.argv[2]
 else:
-    raise NameError
+	raise NameError
 target += id
 
 os.system("/opt/jetbrains/apps/CLion/ch-0/231.9011.31/bin/cmake/linux/x64/bin/cmake --build " + scriptDir + "/cmake-build-debug -j 10 --target " + target)
 command = scriptDir + "/cmake-build-debug/" + target + " local"
 if sys.argv[-1] == "f" or sys.argv[-2] == "f":
-    command += " " + scriptDir
-    if sys.argv[1] == "c":
-        command += "/contests/" + sys.argv[2]
-    else:
-        command += "/freeplay/" + sys.argv[1]
-    command += "/output/output"
-    if sys.argv[-1] == "f":
-        command += id
-    elif sys.argv[-2] == "f":
-        command += sys.argv[-1]
-    command += ".txt"
+	command += " " + scriptDir
+	if sys.argv[1] == "c":
+		command += "/contests/" + sys.argv[2]
+	else:
+		command += "/freeplay/" + sys.argv[1]
+	command += "/output/output"
+	if sys.argv[-1] == "f":
+		command += id
+	elif sys.argv[-2] == "f":
+		command += sys.argv[-1]
+	command += ".txt"
 
 os.system(command)
